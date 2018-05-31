a6359d98877c48e0cedc8f32a34d97bbf97b8232
maxmcd <max.t.mcdonnell@gmail.com>
2018-05-30 21:38:44 -0400

    Json message parsing and Sdp message parse_buffer fix

--- sendrecv/gst-rust/src/main.rs
+++ sendrecv/gst-rust/src/main.rs
@@ -6,9 +6,11 @@ extern crate gstreamer_webrtc;
 extern crate ws;
 #[macro_use]
 extern crate serde_json;
+
 use glib::translate::*;
 use gst::prelude::*;
 use gst::{BinExt, ElementExt};
+use std::mem;
 // use std::io;
 
 #[derive(PartialEq, Eq)]
@@ -36,6 +38,7 @@ enum AppState {
 struct WsClient {
     out: ws::Sender,
     app_state: AppState,
+    webrtc: Option<gst::Element>,
 }
 
 fn check_plugins() -> bool {
@@ -80,7 +83,7 @@ fn sdp_message_as_text(offer: gstreamer_webrtc::WebRTCSessionDescription) -> Opt
     }
 }
 
-fn send_sdp_offer(offer: gstreamer_webrtc::WebRTCSessionDescription, out: ws::Sender) {
+fn send_sdp_offer(offer: gstreamer_webrtc::WebRTCSessionDescription, out: &ws::Sender) {
     let message = json!({
           "sdp": {
             "type": "offer",
@@ -90,7 +93,7 @@ fn send_sdp_offer(offer: gstreamer_webrtc::WebRTCSessionDescription, out: ws::Se
     out.send(message.to_string()).unwrap();
 }
 
-fn on_offer_created(webrtc: gst::Element, promise: &gst::Promise, out: ws::Sender) {
+fn on_offer_created(webrtc: gst::Element, promise: &gst::Promise, out: &ws::Sender) {
     let reply = promise.get_reply().unwrap();
 
     println!("{:?}", reply);
@@ -111,13 +114,14 @@ fn on_offer_created(webrtc: gst::Element, promise: &gst::Promise, out: ws::Sende
     send_sdp_offer(offer, out)
 }
 
-fn on_negotiation_needed(values: &[glib::Value], out: ws::Sender) -> Option<glib::Value> {
+fn on_negotiation_needed(values: &[glib::Value], out: &ws::Sender) -> Option<glib::Value> {
     println!("on-negotiation-needed {:?}", values);
     let webrtc = values[0].get::<gst::Element>().expect("Invalid argument");
     let webrtc_clone = webrtc.clone();
+    let out_clone = out.clone();
     println!("{:?}", webrtc);
     let promise = gst::Promise::new_with_change_func(move |promise: &gst::Promise| {
-        on_offer_created(webrtc, promise, out);
+        on_offer_created(webrtc, promise, &out_clone);
     });
     let options = gst::Structure::new_empty("options");
     let desc = webrtc_clone
@@ -142,7 +146,7 @@ fn send_ice_candidate_message(values: &[glib::Value], out: &ws::Sender) -> Optio
     None
 }
 
-fn start_pipeline(out: &ws::Sender) -> AppState {
+fn start_pipeline(out: &ws::Sender) -> gst::Element {
     let pipeline = gst::parse_launch(
         "webrtcbin name=sendrecv
         stun-server=stun://stun.l.google.com:19302
@@ -168,8 +172,7 @@ fn start_pipeline(out: &ws::Sender) -> AppState {
     let out_clone = out.clone();
     webrtc
         .connect("on-negotiation-needed", false, move |values| {
-            let out = out_clone.clone();
-            on_negotiation_needed(values, out)
+            on_negotiation_needed(values, &out_clone)
         })
         .unwrap();
 
@@ -183,7 +186,20 @@ fn start_pipeline(out: &ws::Sender) -> AppState {
     let ret = pipeline.set_state(gst::State::Playing);
     assert_ne!(ret, gst::StateChangeReturn::Failure);
 
-    return AppState::None;
+    return webrtc;
+}
+
+fn gst_sdp_message_parse_buffer(data: &[u8]) -> gstreamer_sdp::SDPMessage {
+    unsafe {
+        let mut sdp = mem::zeroed();
+        gstreamer_sdp_sys::gst_sdp_message_new(&mut sdp);
+        gstreamer_sdp_sys::gst_sdp_message_parse_buffer(
+            data.to_glib_none().0 as *mut u8,
+            data.len() as u32,
+            sdp,
+        );
+        from_glib_full(sdp)
+    }
 }
 
 impl ws::Handler for WsClient {
@@ -196,20 +212,63 @@ impl ws::Handler for WsClient {
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
         // Close the connection when we get a response from the server
         println!("Got message: {}", msg);
-        let new_state = match format!("{}", msg).as_ref() {
+        let msg_text = msg.into_text().unwrap();
+        let matched = match msg_text.as_ref() {
             "HELLO" => {
                 self.app_state = AppState::ServerRegistered;
-                setup_call(&self.out)
+                setup_call(&self.out);
+                true
             }
             "SESSION_OK" => {
                 self.app_state = AppState::PeerConnected;
-                start_pipeline(&self.out)
+                self.webrtc = Some(start_pipeline(&self.out));
+                true
             }
-            _ => AppState::None,
+            _ => false,
         };
-        if new_state != AppState::None {
-            self.app_state = new_state;
+        if matched {
+            return Ok(());
         }
+
+        if msg_text.starts_with("ERROR") {
+            println!("Got error message! {}", msg_text);
+            // TODO: err & quit?
+            return Ok(());
+        }
+
+        let json_msg: serde_json::Value = serde_json::from_str(&msg_text).unwrap();
+        if json_msg.get("sdp").is_some() {
+            if !json_msg["sdp"].get("type").is_some() {
+                println!("ERROR: received SDP without 'type'");
+                return Ok(());
+            }
+            let sdptype = &json_msg["sdp"]["type"];
+            assert_eq!(sdptype, "answer");
+            let text = &json_msg["sdp"]["sdp"];
+            print!("Received answer:\n{}\n", text.as_str().unwrap());
+
+            let ret = gst_sdp_message_parse_buffer(text.as_str().unwrap().as_bytes());
+            println!("{:?}", ret.as_text());
+
+            println!("DONE");
+            let answer = gstreamer_webrtc::WebRTCSessionDescription::new(
+                gstreamer_webrtc::WebRTCSDPType::Answer,
+                ret,
+            );
+            let promise = gst::Promise::new();
+            self.webrtc
+                .as_ref()
+                .unwrap()
+                .emit(
+                    "set-remote-description",
+                    &[&answer.to_value(), &promise.to_value()],
+                )
+                .unwrap();
+        }
+        if json_msg.get("ice").is_some() {
+            println!("ice {:?}", json_msg);
+        }
+
         Ok(())
         // self.out.close(ws::CloseCode::Normal)
     }
@@ -219,6 +278,7 @@ fn connect_to_websocket_server_async() {
     let result = ws::connect("ws://signalling:8443", |out| WsClient {
         out: out,
         app_state: AppState::AppStateUnknown,
+        webrtc: None,
     });
     match result {
         Ok(_) => {
